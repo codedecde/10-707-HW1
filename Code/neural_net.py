d626bb0670cbbafe312715748587811ebb1fd9b2
codedecde <barunpatra95@gmail.com>
2017-09-22 18:46:51 -0400

    Plotting convenience

--- Code/neural_net.py
+++ Code/neural_net.py
@@ -1,18 +1,15 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
 from utils import Progbar
-from optimizers import optimizer
+from optimizers import SGD
 from Layers import DenseLayer, BatchNormLayer
 from copy import deepcopy
 import pdb
 np.random.seed(1234)
 
 
-DEBUG = False
-
-
 class neural_net(object):
-    def __init__(self, input_dims, layers_info):
+    def __init__(self, input_dims, layers_info, opts):
         self.layers_info = layers_info
         self.num_layers = len(layers_info)
         self.params = {}
@@ -22,13 +19,13 @@ class neural_net(object):
             else:
                 input_dim = layers_info[ix - 1][1]
             output_dim = layers_info[ix][1]
-            if layer_info[ix][0] != "batchnorm":
-                layer_object = DenseLayer(input_dim, output_dim, layers_info[ix][2], dropout=layer_info[ix][3])
+            if layers_info[ix][0] != "batchnorm":
+                layer_object = DenseLayer(input_dim, output_dim, layers_info[ix][2], dropout=layers_info[ix][3])
             else:
                 layer_object = BatchNormLayer(input_dim)
             self.params[layers_info[ix][0] + "_{}".format(ix)] = layer_object.params
             setattr(self, 'layer_{}'.format(ix), layer_object)
-        self.optimizer = optimizer(self.params, 'categorical_cross_entropy', lr=0.1, l2_penalty=0)
+        self.optimizer = SGD(self.params, 'categorical_cross_entropy', lr=opts.lr, l2_penalty=opts.l2)
 
     def forward(self, input_tensor, test=False):
         output = input_tensor
@@ -60,59 +57,56 @@ class neural_net(object):
                 max_abs_difference = max(abs_difference, max_abs_difference)
         return max_abs_difference
 
-    def train_batch(self, X, y):
+    def train_batch(self, X, y, X_val, y_val):
         self.optimizer.zero_grads()
         output = self.forward(X)
         loss, loss_grad = self.optimizer.loss(y, output)
         self.backward(loss_grad)
         # print self.test_layer_gradient('hidden_0', 'b', X, y)
         self.optimizer.step()
-        return loss
+        loss_val, _ = self.optimizer.loss(y_val, self.forward(X_val))
+        return loss, loss_val
 
     def predict(self, X):
         output = self.forward(X, test=True)
         output = np.argmax(output, axis=-1)
         return output
 
-    def fit(self, X, y, X_val, y_val, n_epochs=200, batch_size=32):
-        y_val = np.argmax(y_val, axis=-1)
+    def fit(self, X_train, y_train, X_val, y_val, n_epochs=200, batch_size=32, return_history=False):
+        y_labels_val = np.argmax(y_val, axis=-1)
+        y_labels_train = np.argmax(y_train, axis=-1)
         bar = Progbar(n_epochs)
+        if return_history:
+            history = {'train_loss':[], 'val_loss':[], 'train_acc':[], 'val_acc':[]}
         for epoch in xrange(n_epochs):
             # Shuffle the training data
-            index = np.arange(X.shape[0])
+            index = np.arange(X_train.shape[0])
             np.random.shuffle(index)
-            X = X[index]
-            y = y[index]
+            X = X_train[index]
+            y = y_train[index]
+            train_loss = 0.
+            val_loss = 0.
             losses = []
             for ix in xrange(0, X.shape[0], batch_size):
                 batch_x = X[ix: ix + batch_size]
                 batch_y = y[ix: ix + batch_size]
-                loss = self.train_batch(batch_x, batch_y)
-                losses.append(loss)
-            preds = self.predict(X_val)
-            bar.update(epoch + 1, values=[("mean_training_loss", sum(losses) / len(losses)), ("val_acc", accuracy_score(y_val, preds))])
+                loss_train, loss_val = self.train_batch(batch_x, batch_y, X_val, y_val)
+                train_loss += loss_train * batch_x.shape[0]
+                val_loss += loss_val * batch_x.shape[0]
+                losses.append(loss_train)
+            train_loss /= X.shape[0]
+            val_loss /= X.shape[0]
+            train_acc = accuracy_score(y_labels_train, self.predict(X_train))
+            val_acc = accuracy_score(y_labels_val, self.predict(X_val))
+            if return_history:
+                history['train_loss'].append(train_loss)
+                history['val_loss'].append(val_loss)
+                history['train_acc'].append(train_acc)
+                history['val_acc'].append(val_acc)
+            bar.update(epoch + 1, values=[("train_loss", train_loss),
+                                          ("val_loss", val_loss),
+                                          ("train_acc", train_acc),
+                                          ("val_acc", val_acc)])
+        if return_history:
+            return history
 
-
-def get_x_y(data_array):
-    x = data_array[:, :-1]
-    y = data_array[:, -1].reshape(data_array.shape[0], 1)
-    y = np.array(data_array[:, -1], dtype='int').ravel()
-    num_classes = np.max(y) + 1
-    categorical_y = np.zeros((x.shape[0], num_classes))
-    categorical_y[np.arange(x.shape[0]), y] = 1
-    return x, categorical_y
-
-
-if __name__ == "__main__":
-    train_file = "Data/digitstrain.txt"
-    val_file = "Data/digitsvalid.txt"
-    train_x, train_y = get_x_y(np.genfromtxt(train_file, delimiter=","))
-    val_x, val_y = get_x_y(np.genfromtxt(val_file, delimiter=","))
-    # layer_info = [("hidden", 100, "relu", 0.5), ("batchnorm", 100, "", 0.), ("output", 10, "softmax", 1.)]
-    # layer_info = [("hidden", 100, "tanh", .5), ("batchnorm", 100), ("hidden", 100, "tanh", .5), ("batchnorm", 100), ("output", 10, "softmax", 1.)]
-    # layer_info = [("hidden", 5, "relu", 1.), ("batchnorm", 5), ("hidden", 5, "tanh", 1.), ("output", 10, "softmax", 1.)]
-    # layer_info = [("hidden", 100, "relu", .5), ("batchnorm", 100), ("hidden", 100, "relu", .5), ("output", 10, "softmax", 1.)]
-    layer_info = [("hidden", 100, "relu", 1.), ("batchnorm", 100), ("hidden", 100, "relu", 1.), ("output", 10, "softmax", 1.)]
-    # layer_info = [("hidden", 100, "relu", 1.), ("output", 10, "softmax", 1.)]
-    nn = neural_net(train_x.shape[1], layer_info)
-    nn.fit(train_x, train_y, val_x, val_y)
---
+++ Code/run_net.py
@@ -0,0 +1,68 @@
+from neural_net import neural_net
+import numpy as np
+import argparse
+import sys
+import pdb
+import cPickle as cp
+
+
+def get_x_y(data_array):
+    x = data_array[:, :-1]
+    y = data_array[:, -1].reshape(data_array.shape[0], 1)
+    y = np.array(data_array[:, -1], dtype='int').ravel()
+    num_classes = np.max(y) + 1
+    categorical_y = np.zeros((x.shape[0], num_classes))
+    categorical_y[np.arange(x.shape[0]), y] = 1
+    return x, categorical_y
+
+
+def get_arguments():
+    def check_boolean(args, attr_name):
+        assert hasattr(args, attr_name), "%s not found in parser" % (attr_name)
+        bool_set = set(["true", "false"])
+        args_value = getattr(args, attr_name)
+        args_value = args_value.lower()
+        assert args_value in bool_set, "Boolean argument required for attribute %s" % (attr_name)
+        args_value = False if args_value == "false" else True
+        setattr(args, attr_name, args_value)
+        return args
+
+    parser = argparse.ArgumentParser(description='Basic Neural Network')
+    parser.add_argument('-n_hidden', action="store", default=100, dest="n_hidden", type=int)
+    parser.add_argument('-batch', action="store", default=32, dest="batch_size", type=int)
+    parser.add_argument('-dropout', action="store", default=1.0, dest="dropout", type=float)
+    parser.add_argument('-l2', action="store", default=0.000, dest="l2", type=float)
+    parser.add_argument('-lr', action="store", default=0.1, dest="lr", type=float)
+    parser.add_argument('-momentum', action="store", default=0.0, dest="momentum", type=float)
+    parser.add_argument('-activation', action="store", default="sigmoid", dest="activation", type=str)
+    parser.add_argument('-n_epochs', action="store", default=200, dest="n_epochs", type=int)
+    # Using strings as a proxy for boolean flags. Checks happen later
+    args = parser.parse_args(sys.argv[1:])
+    # Checks for the boolean flags
+    return args
+
+
+if __name__ == "__main__":
+    opts = get_arguments()
+    train_file = "Data/digitstrain.txt"
+    val_file = "Data/digitsvalid.txt"
+    train_x, train_y = get_x_y(np.genfromtxt(train_file, delimiter=","))
+    val_x, val_y = get_x_y(np.genfromtxt(val_file, delimiter=","))
+    layer_info = [("hidden", opts.n_hidden, "sigmoid", opts.dropout), ("output", 10, "softmax", 1.)]
+    # layer_info = [("hidden", 100, "relu", 0.5), ("batchnorm", 100, "", 0.), ("output", 10, "softmax", 1.)]
+    # layer_info = [("hidden", 100, "tanh", .5), ("batchnorm", 100), ("hidden", 100, "tanh", .5), ("batchnorm", 100), ("output", 10, "softmax", 1.)]
+    # layer_info = [("hidden", 5, "relu", 1.), ("batchnorm", 5), ("hidden", 5, "tanh", 1.), ("output", 10, "softmax", 1.)]
+    # layer_info = [("hidden", 100, "relu", .5), ("batchnorm", 100), ("hidden", 100, "relu", .5), ("output", 10, "softmax", 1.)]
+    # layer_info = [("hidden", 100, "relu", 1.), ("batchnorm", 100), ("hidden", 100, "relu", 1.), ("output", 10, "softmax", 1.)]
+    # layer_info = [("hidden", 100, "relu", 1.), ("output", 10, "softmax", 1.)]
+    nn = neural_net(train_x.shape[1], layer_info, opts)
+    history = nn.fit(train_x, train_y, val_x, val_y, n_epochs=opts.n_epochs, batch_size=opts.batch_size, return_history=True)
+    history_file_name = 'History_hidden_%d_dropout_%.2f_batch_%d_l2_%.4f_lr_%.3f_momentum_%.3f_activation_%s_epochs_%d.pkl' % (opts.n_hidden,
+                                                                                                                               opts.dropout,
+                                                                                                                               opts.batch_size,
+                                                                                                                               opts.l2,
+                                                                                                                               opts.lr,
+                                                                                                                               opts.momentum,
+                                                                                                                               opts.activation,
+                                                                                                                               opts.n_epochs)
+    cp.dump(history, open('History/' + history_file_name, 'wb'))

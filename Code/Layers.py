Fixed batch norm. Added exponential averaging for mean and var

Code/Layers.py
@@ -75,18 +75,19 @@ class DenseLayer(object):
 
 
 class BatchNormLayer(object):
-    def __init__(self, n_dim, epsilon=0.001):
+    def __init__(self, n_dim, epsilon=0.001, alpha=0.9):
         self.n_dim = n_dim
         l_val = np.sqrt(6) / (np.sqrt(n_dim + n_dim))
         self.params = {"gamma": Variable(), "beta": Variable()}
         self.params["gamma"].value = np.random.uniform(low=-l_val, high=l_val, size=(1, n_dim))
         self.params["beta"].value = np.random.uniform(low=-l_val, high=l_val, size=(1, n_dim))
         self.buffers = {}
-        self.buffers["mu"] = np.zeros((1, n_dim))
-        self.buffers["sigma"] = np.zeros((1, n_dim))
-        self.sum_of_squares = np.zeros((1, n_dim))
-        self.count = 0.
+        self.buffers["batch_mu"] = np.zeros((1, n_dim))
+        self.buffers["batch_var"] = np.zeros((1, n_dim))
+        self.buffers["mu"] = np.zeros((1, n_dim))  # The running average for mean
+        self.buffers["var"] = np.zeros((1, n_dim))  # The running average for variance
         self.epsilon = epsilon
+        self.alpha = alpha
 
     def forward(self, input_tensor, test=False):
         '''
@@ -94,16 +95,18 @@ class BatchNormLayer(object):
             Returns a tensor of form batch x n_dim
         '''
         if not test:
-            batch_mu = np.sum(input_tensor, axis=0)  # 1 x n_dim
-            batch_sum_of_squares = np.sum(input_tensor * input_tensor, axis=0)
-            self.buffers["mu"] = ((self.buffers["mu"] * self.count) + batch_mu) / (self.count + input_tensor.shape[0])
-            self.sum_of_squares = ((self.sum_of_squares * self.count) + batch_sum_of_squares) / (self.count + input_tensor.shape[0])
-            self.count += input_tensor.shape[0]
-            self.buffers["sigma"] = self.sum_of_squares - (self.buffers["mu"] * self.buffers["mu"])
-        self.numerator = (input_tensor - self.buffers["mu"])
-        self.denominator = self.buffers["sigma"] + self.epsilon
-        self.input_tensor_hat = self.numerator / (np.sqrt(self.denominator))
-        return (self.input_tensor_hat * self.params["gamma"].value) + self.params["beta"].value
+            self.buffers["batch_mu"] = (np.sum(input_tensor, axis=0)) / (input_tensor.shape[0])
+            self.buffers["mu"] = (1. - self.alpha) * self.buffers["mu"] + (self.alpha * self.buffers["batch_mu"])
+            self.buffers["batch_var"] = (np.sum(input_tensor ** 2, axis=0) / input_tensor.shape[0]) - (self.buffers["batch_mu"] ** 2)
+            self.buffers["var"] = (1. - self.alpha) * self.buffers["var"] + (self.alpha * self.buffers["batch_var"])
+        else:
+            self.buffers["batch_mu"] = self.buffers["mu"]
+            self.buffers["batch_var"] = self.buffers["var"]
+        self.x_minus_mu = input_tensor - self.buffers["batch_mu"]
+        self.var_plus_eps = self.buffers["batch_var"] + self.epsilon
+        self.x_hat = self.x_minus_mu / np.sqrt(self.var_plus_eps)
+        output_tensor = (self.x_hat * self.params["gamma"].value) + self.params["beta"].value
+        return output_tensor
 
     def backward(self, output_gradient):
         '''

Code/neural_net.py
@@ -6,6 +6,9 @@ from Layers import DenseLayer, BatchNormLayer
 np.random.seed(1234)
 
 
+DEBUG = False
+
+
 class neural_net(object):
     def __init__(self, input_dims, layers_info):
         self.layers_info = layers_info
@@ -25,39 +28,49 @@ class neural_net(object):
             setattr(self, 'layer_{}'.format(ix), layer_object)
         self.optimizer = optimizer(self.params, 'categorical_cross_entropy', lr=0.1, l2_penalty=0)
 
-    def forward_prop(self, input_tensor, test=False):
+    def forward(self, input_tensor, test=False):
         output = input_tensor
         for ix in xrange(self.num_layers):
             output = getattr(self, 'layer_{}'.format(ix))(output, test=test)
         return output
 
-    def backward_prop(self, loss_grad):
+    def backward(self, loss_grad):
         back_grad = loss_grad
         for ix in xrange(self.num_layers - 1, -1, -1):
             back_grad = getattr(self, 'layer_{}'.format(ix)).backward(back_grad)
 
+    def compute_numerical_grad(self, layer, param, i, j, X, y, eps=0.0001):
+        original_params = deepcopy(self.params[layer][param].value)
+        self.params[layer][param].value[i][j] = original_params[i][j] + eps
+        loss_pos, _ = self.optimizer.loss(y, self.forward(X))
+        self.params[layer][param].value[i][j] = original_params[i][j] - eps
+        loss_neg, _ = self.optimizer.loss(y, self.forward(X))
+        num_grad = (loss_pos - loss_neg) / (2 * eps)
+        self.params[layer][param].value = original_params
+        return num_grad
+
     def train_batch(self, X, y):
         self.optimizer.zero_grads()
-        output = self.forward_prop(X)
+        output = self.forward(X)
         loss, loss_grad = self.optimizer.loss(y, output)
-        self.backward_prop(loss_grad)
+        self.backward(loss_grad)
         self.optimizer.step()
         return loss
 
     def predict(self, X):
-        output = self.forward_prop(X, test=True)
+        output = self.forward(X, test=True)
         output = np.argmax(output, axis=-1)
         return output
 
-    def fit(self, X, y, X_val, y_val, n_epochs=2000, batch_size=50):
-        # Shuffle the training data
-        index = np.arange(X.shape[0])
-        np.random.shuffle(index)
-        X = X[index]
-        y = y[index]
+    def fit(self, X, y, X_val, y_val, n_epochs=200, batch_size=32):
         y_val = np.argmax(y_val, axis=-1)
         bar = Progbar(n_epochs)
         for epoch in xrange(n_epochs):
+            # Shuffle the training data
+            index = np.arange(X.shape[0])
+            np.random.shuffle(index)
+            X = X[index]
+            y = y[index]
             losses = []
             for ix in xrange(0, X.shape[0], batch_size):
                 batch_x = X[ix: ix + batch_size]
@@ -84,8 +97,10 @@ if __name__ == "__main__":
     train_x, train_y = get_x_y(np.genfromtxt(train_file, delimiter=","))
     val_x, val_y = get_x_y(np.genfromtxt(val_file, delimiter=","))
     # layer_info = [("hidden", 100, "relu", 0.5), ("batchnorm", 100, "", 0.), ("output", 10, "softmax", 1.)]
-    layer_info = [("hidden", 100, "tanh", .5), ("batchnorm", 100), ("hidden", 100, "tanh", .5), ("batchnorm", 100), ("output", 10, "softmax", 1.)]
+    # layer_info = [("hidden", 100, "tanh", .5), ("batchnorm", 100), ("hidden", 100, "tanh", .5), ("batchnorm", 100), ("output", 10, "softmax", 1.)]
+    # layer_info = [("hidden", 5, "relu", 1.), ("batchnorm", 5), ("hidden", 5, "tanh", 1.), ("output", 10, "softmax", 1.)]
+    # layer_info = [("hidden", 100, "relu", .5), ("batchnorm", 100), ("hidden", 100, "relu", .5), ("output", 10, "softmax", 1.)]
     # layer_info = [("hidden", 100, "relu", .5), ("hidden", 100, "relu", .5), ("output", 10, "softmax", 1.)]
-    # layer_info = [("hidden", 100, "relu", 1.), ("output", 10, "softmax", 1.)]
+    layer_info = [("hidden", 100, "relu", 1.), ("output", 10, "softmax", 1.)]
     nn = neural_net(train_x.shape[1], layer_info)
     nn.fit(train_x, train_y, val_x, val_y)
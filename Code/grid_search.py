386fc0cf86f65b170ab4ec81e4be8d3ddbfc4942
codedecde <barunpatra95@gmail.com>
2017-09-23 21:13:34 -0400

    grid seach completed

--- Code/grid_search.py
+++ Code/grid_search.py
@@ -31,8 +31,8 @@ if __name__ == "__main__":
     train_x, train_y = get_x_y(np.genfromtxt(train_file, delimiter=","))
     val_x, val_y = get_x_y(np.genfromtxt(val_file, delimiter=","))
     layer_units = [50, 100, 200, 500]
-    lr_values = [0.1, 0.5, 0.01, 0.005]
-    num_layers = 1
+    lr_values = [0.1, 0.05]
+    num_layers = 2
     l2_low = 0.0001
     l2_high = 0.01
     momentum_low = 0.6
@@ -63,4 +63,39 @@ if __name__ == "__main__":
                     best_val_acc = best_val_acc_model
                     best_save_string = model_save_prefix
                 cp.dump(history, open(data_dir + history_file_name, 'wb'))
+    else:
+        for ix in xrange(len(layer_units)):
+            unit_1 = layer_units[ix]
+            for jx in xrange(ix + 1):
+                unit_2 = layer_units[jx]
+                opts.n_hidden = unit_1
+                layers_info = [('hidden', unit_1, opts.activation, 1.), ('hidden', unit_2, opts.activation, 1.), ("output", 10, "softmax", 1.)]
+                for _ in xrange(models_per_unit):
+                    opts.lr = random.choice(lr_values)
+                    opts.l2 = float(format(np.random.uniform(low=l2_low, high=l2_high), '.5f'))
+                    opts.momentum = float(format(np.random.uniform(low=momentum_low, high=momentum_high), '.3f'))
+                    print_string = '\nNUM HIDDEN UNITS 1 %d' % (unit_1)
+                    print_string += '\nNUM HIDDEN UNITS 2 %d' % (unit_2)
+                    print_string += "\nLR %.4f" % (opts.lr)
+                    print_string += "\nL2 %.5f" % (opts.l2)
+                    print_string += "\nMomentum %.3f" % (opts.momentum)
+                    print print_string
+                    model_save_prefix = 'Parameters/Model_hidden1_%d_hidden2_%d_l2_%.4f_lr_%.3f_momentum_%.3f' % (unit_1,
+                                                                                                                  unit_2,
+                                                                                                                  opts.l2,
+                                                                                                                  opts.lr,
+                                                                                                                  opts.momentum)
+                    history_file_name = 'History/History_hidden1_%d_hidden2_%d_l2_%.4f_lr_%.3f_momentum_%.3f.pkl' % (unit_1,
+                                                                                                                     unit_2,
+                                                                                                                     opts.l2,
+                                                                                                                     opts.lr,
+                                                                                                                     opts.momentum)
+                    opts.save_prefix = data_dir + model_save_prefix
+                    nnet = nn.neural_net(train_x.shape[1], layers_info, opts)
+                    history = nnet.fit(train_x, train_y, val_x, val_y, n_epochs=opts.n_epochs, batch_size=opts.batch_size, return_history=True)
+                    best_val_acc_model = history['best_val_acc']
+                    if best_val_acc is None or best_val_acc_model > best_val_acc:
+                        best_val_acc = best_val_acc_model
+                        best_save_string = model_save_prefix
+                    cp.dump(history, open(data_dir + history_file_name, 'wb'))
     print '\n\n Best Validation Accuracy Achieved : %.4f\nBy Model File\n%s\n' % (best_val_acc, best_save_string)
